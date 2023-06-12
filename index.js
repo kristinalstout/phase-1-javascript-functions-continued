5f0bea1ecc3558c4030d79f7defd6b96c8ef04c9
Kristina Stout <kristinalstout@gmail.com>
2023-06-12 16:20:09 -0400

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(task = "go to the office"){
+    return `This Monday, I will ${task}.`;
+}
+
+function wrapAdjective(visualFlare = "*") {
+    const sentence = function (adjective = "special") {
+      return `You are ${visualFlare}${adjective}${visualFlare}!`;
+    };
+    return sentence;
+}
